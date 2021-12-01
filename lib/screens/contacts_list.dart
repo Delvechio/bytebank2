a448522aede5785ccd8881f0ac2dfa7d529b1503
Delvechio Duarte da Silva Júnior <delvechioduarte2502@gmail.com>
2021-12-01 10:24:43 -0300

    Initial commit

---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ContactList extends StatelessWidget {
+  const ContactList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text(
+                'Delvechio',
+                style: TextStyle(fontSize: 24),
+              ),
+              subtitle: Text(
+                '1000',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
